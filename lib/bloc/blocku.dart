6d5d83725f94890d48ce61a70a6ef913be167d7d
thekill022 <gibfathoni@gmail.com>
2026-04-03 22:11:07 +0700

    membuat class EmailChanged

--- lib/bloc/blocku.dart
+++ lib/bloc/blocku.dart
@@ -4,3 +4,8 @@ class NamaChanged extends FormEvent {
   final String nama;
   NamaChanged(this.nama);
 }
+
+class EmailChanged extends FormEvent {
+  final String email;
+  EmailChanged(this.email);
+}

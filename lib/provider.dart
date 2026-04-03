fbbbd58747cb3f98235c057a47bbcbe630f24a5f
thekill022 <gibfathoni@gmail.com>
2026-04-03 21:19:27 +0700

    membuat class FormProvider

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,6 +1,22 @@
 
 import 'package:flutter/cupertino.dart';
 
+class FormProvider extends ChangeNotifier {
+  String nama = "";
+  String email = "";
+
+  void setNama(String value) {
+    nama = value;
+    notifyListeners();
+  }
+
+  void setEmail(String value) {
+    email = value;
+    notifyListeners();
+  }
+
+}
+
 class FormPage extends StatelessWidget {
   const FormPage({super.key});
 

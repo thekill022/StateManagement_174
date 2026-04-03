7b9a0c3829cc705d1a1aceb1757de3b628528d26
thekill022 <gibfathoni@gmail.com>
2026-04-03 22:04:43 +0700

    menambahkan text field disertai text controller variable untuk setstate.dart

--- lib/setstate/setstate.dart
+++ lib/setstate/setstate.dart
@@ -25,6 +25,29 @@ class _FormPageState extends State<FormPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Form Input"),),
+      body: Padding(
+          padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: namaController,
+              decoration: InputDecoration(
+                labelText: "Nama",
+                border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 20,),
+          ],
+        ),
+      ),
     );
   }
 }
